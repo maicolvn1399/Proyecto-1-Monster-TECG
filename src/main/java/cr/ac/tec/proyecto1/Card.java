b365a5b59bb216931f94b7ebe7318330e5fb576a
igmochang <ig.morales@estudiantec.cr>
2020-10-27 12:02:19 -0600

    Creación de clases Card y Deck

--- src/main/java/cr/ac/tec/proyecto1/Card.java
+++ src/main/java/cr/ac/tec/proyecto1/Card.java
@@ -1,4 +1,23 @@
 package cr.ac.tec.proyecto1;
 
 public class Card {
+
+    public Card(Value value) {
+        this.value = value;
+    }
+
+    enum Value {
+        Spell1, Spell2, Spell3, Spell4, Spell5, Spell6, Spell7, Spell8, Spell9, Spell10, Esbirro1, Esbirro2, Esbirro3, Esbirro4, Esbirro5, Esbirro6, Esbirro7, Esbirro8, Esbirro9, Esbirro10, Secret1, Secret2, Secret3, Secret4, Secret5, Secret6, Secret7, Secret8, Secret9, Secret10;
+
+        private static final Value[] values = Value.values();
+        public static Value getValue(int i){
+            return Value.values[i];
+        }
+    }
+    private final Value value;
+
+    public Value getValue() {
+        return this.value;
+    }
+
 }
---
+++ src/main/java/cr/ac/tec/proyecto1/Deck.java
@@ -0,0 +1,49 @@
+package cr.ac.tec.proyecto1;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+    private Card[] cards;
+    private int cardsInDeck;
+
+    public Deck(){
+        cards = new Card[30];
+    }
+
+    public void reset(){
+        cardsInDeck = 0;
+        Card.Value[] values = Card.Value.values();
+        for (int i = 0; i < values.length; i++){
+            Card.Value value = values[i];
+            cards[cardsInDeck] = new Card(value);
+        }
+    }
+
+    public void replaceDeckWith(ArrayList<Card> cards) {
+        this.cards = cards.toArray(new Card[cards.size()]);
+        this.cardsInDeck = this.cards.length;
+    }
+
+    public boolean isEmpty() {
+        return cardsInDeck == 0;
+    }
+
+    public void shuffle(){
+        int n = cards.length;
+        Random random = new Random();
+
+        for (int i = 0; i < cards.length; i++){
+            int randomValue = i + random.nextInt(n - i);
+            Card randomCard = cards[randomValue];
+            cards[randomValue] = cards[i];
+            cards[i] = randomCard;
+        }
+    }
+   public Card drawCard() throws IllegalArgumentException{
+        if (isEmpty()){
+            throw new IllegalArgumentException("No hay cartas para comer");
+        }
+        return cards[--cardsInDeck];
+   }
+}
