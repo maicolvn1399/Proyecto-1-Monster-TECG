64128a9cef802e4b0bdfcbfb7b56861ea70eeb37
igmochang <ig.morales@estudiantec.cr>
2020-10-31 10:08:42 -0600

    Creación de lista doblemente enlazada

---
+++ src/main/java/cr/ac/tec/proyecto1/linearStructures/BattleLog.java
@@ -0,0 +1,68 @@
+package cr.ac.tec.proyecto1.linearStructures;
+
+public class BattleLog {
+    Node head;
+
+    class Node {
+        Object data;
+        Node prev;
+        Node next;
+
+        Node(Object d) { data = d; }
+    }
+
+    public void push(Object new_data) {
+        Node new_Node = new Node(new_data);
+
+
+        new_Node.next = head;
+        new_Node.prev = null;
+
+        if (head != null)
+            head.prev = new_Node;
+
+        head = new_Node;
+    }
+
+
+    void append(Object new_data) {
+        Node new_node = new Node(new_data);
+
+        Node last = head;
+
+        new_node.next = null;
+
+        if (head == null) {
+            new_node.prev = null;
+            head = new_node;
+            return;
+        }
+
+        while (last.next != null)
+            last = last.next;
+
+
+        last.next = new_node;
+
+        new_node.prev = last;
+    }
+
+    public void printlist(Node node) {
+        Node last = null;
+        System.out.println("Primero a último");
+        while (node != null) {
+            System.out.print(node.data + " ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println();
+        System.out.println("Último a primero");
+        while (last != null) {
+            System.out.print(last.data + " ");
+            last = last.prev;
+        }
+    }
+}
+
+
+
